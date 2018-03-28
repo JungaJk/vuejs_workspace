79597fcd3133ed0ef4608f943d9dd3bd9b36fa3c
JungaJK <wjddk0929@gmail.com>
2018-03-28 15:42:31 +0100

    add server component

---
+++ 02.ComponentCommunicationExercise/src/components/Server/Server.vue
@@ -0,0 +1,20 @@
+<template>
+    <li class="list-group-item"  @click="serverSelected">
+        Server #{{ id }}
+    </li>
+</template>
+
+<script>
+    export default{
+       props:['id'],
+        methods:{
+            serverSelected(){
+                console.log('sss')
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
